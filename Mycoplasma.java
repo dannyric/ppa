4962944c27797f9ecbc3156fd33e36074df600c4
js41 <86107569+dannyric@users.noreply.github.com>
2024-02-07 00:27:35 +0000

    Update Mycoplasma.java

--- Mycoplasma.java
+++ Mycoplasma.java
@@ -23,16 +23,31 @@ public class Mycoplasma extends Cell {
         super(field, location, col);
     }
 
+    
+    // Ran a few times and seems to be working fine
     /**
     * This is how the Mycoplasma decides if it's alive or not
     */
     public void act() {
+        /*
         List<Cell> neighbours = getField().getLivingNeighbours(getLocation());
         setNextState(false);
-    
         if (isAlive()) {
             if (neighbours.size() > 1)
                 setNextState(true);
+            }
+        */
+       
+        // Dies if fewer than two of its neighbours are alive
+        if (numberOfAliveNeighbours()<2 || numberOfAliveNeighbours()>3)
+        {
+            setNextState(false);
         }
+        // Cell is alive if exactly 3 neighbours are alive
+        else if (numberOfAliveNeighbours()==3)
+        {
+            setNextState(true);
+        }
+        // Stays in current state if exactly 2 neighbours are alive (or any other situation than those mentioned above)
     }
 }
