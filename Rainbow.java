bba646505aaa8138c2cce3d4c7de711cac8a27c9
flynnle <108338937+flynnle@users.noreply.github.com>
2024-02-28 11:40:06 +0000

    Update Rainbow.java

--- Rainbow.java
+++ Rainbow.java
@@ -1,5 +1,6 @@
 import javafx.scene.paint.Color; 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Write a description of class Rainbow here.
@@ -9,36 +10,46 @@ import java.util.List;
  */
 public class Rainbow extends Cell
 {
-    // Constructor for rainbow
+    // instance variables - replace the example below with your own
+    //Constructor for rainbow
+
     public Rainbow(Field field, Location location, Color col) {
         super(field, location, col);
     }
 
     /**
-    * This is how the rainbow cell decides if it's alive or not
-    */
-      public void act() {
+     * This is how the Mycoplasma decides if it's alive or not
+     */
+    public void act() {
         int numberOfAliveNeighbours = getNumberOfAliveNeighbours();
-        
-        if(numberOfAliveNeighbours % 2==0){
+        if (getColor() == Color.BLACK){
+            isAlive(); 
+        }
+        else if(numberOfAliveNeighbours % 2==0){
             setColor(Color.GREEN); // sets the colour to green if the num. of neightbours is even 
-            if(numberOfAliveNeighbours > 3){ // deletes if the number of neigbours is greater than 3
-                 setNextState(false);
+            if(numberOfAliveNeighbours > 3){ // deletes it the number of neigbours is greater then 3
+                setNextState(false);
             }
             else{
-                setNextState(true); //reinstates if the number is less than 4
+                setNextState(true); //reinstates if the number is less then 4
             }
         }
         else if(numberOfAliveNeighbours % 2==1){
-             setColor(Color.BLUE); // sets the colour to green if the num. of neightbours is even 
+            setColor(Color.BLUE); // sets the colour to green if the num. of neightbours is even 
             if(numberOfAliveNeighbours < 4){ // reinsates if the number of neigbours is less then 4
-                 setNextState(true);
+                setNextState(true);
             }
             else{
                 setNextState(false); //deletes if the number is greater then 3
             }
-
+        }
+        Random random = Randomizer.getRandom();
+        if((isAlive() == true) && random.nextDouble() <= DISEASED_PROB){ //checks if the cell is alive and if meets the chance the cell is effect by a disease 
             
+          makeDiseased();
         }
     }
+
 }
+
+
